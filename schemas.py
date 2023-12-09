a07b45953c59654954959c481f0f1be391e5a893
AhemdMuqawi <ahmedmuqawi@gmail.com>
2023-12-09 03:10:27 +0200

    create a pydantic models

---
+++ schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from typing import List, Dict, Union
+
+
+# create pydantic model for the collection name
+class IllnessCategory(BaseModel):
+    illness_type: str
+
+
+# create pydantic model for the document name
+class IllnessNames(BaseModel):
+    illness_name: str
+
+
+# create pydantic model for the fields
+class MedicalInformation(BaseModel):
+    Symptoms: List[Union[str, dict]]
+    Red_Flags: List[Union[str, dict]]
+    Initial_Management: List[Union[str, dict]]
+    Do_Or_Not: Dict[str, List[str]]
+
+
+# for discussion
+# # create pydantic model for symptoms field
+
+
+# #create pydantic model for red falgs field
+
+
+# #create pydantic model for initial management field
+
+
+# #create pydantic model for do and not  field
