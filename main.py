8b4d57995109db53acd6d9ad8c72fada2cc02421
AhemdMuqawi <ahmedmuqawi@gmail.com>
2023-12-12 13:18:08 +0200

    add route to get the illness category in main.py file

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from .routers import git, miscellaneous, pediatric, respiratory, urinary
+from . import schemas
+from . import medical_info
 
 
 app = FastAPI()
+
+
+# route that will return the collection name
+@app.get("/diseases", response_model=schemas.IllnessCategory, tags=["Illness Category"])
+def get_category():
+    return medical_info.get_illnesses_types()
+
 
 # include the APIRouters for git, miscellaneous ,etc..
 app.include_router(pediatric.router)
