d7bd08f16cd0c639a62e6bdf57f200cf5041db20
AhemdMuqawi <ahmedmuqawi@gmail.com>
2023-12-30 14:54:51 +0200

    add a function to get the image of a specific category

--- medical_info.py
+++ medical_info.py
@@ -5,6 +5,12 @@ from . import schemas
 db = database.db
 
 
+def get_image(image_name: str) -> str:
+    photos_collection = db["Photos"]
+    image_url = photos_collection.find_one({image_name: {"$exists": True}})
+    return image_url.get(image_name)
+
+
 def get_illnesses_types() -> list[str]:
     collections_list = db.list_collection_names()
     return schemas.IllnessCategory(illness_type=collections_list)
@@ -41,3 +47,4 @@ def get_illness_info(collection_name: str, document_key: str) -> Dict:
 #     print("*" * 50)
 # print(get_illnesses_names("Pediatric_Emergency"))
 # print(get_illness_info("Urinary_Tract_diseases", "Urinary Tract Infection"))
+print(get_image("Pediatric Emergency"))
