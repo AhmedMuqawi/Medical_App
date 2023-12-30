fb9ae3b90d0c66ed39396ce8aa9919c064c45eec
AhemdMuqawi <ahmedmuqawi@gmail.com>
2023-12-30 15:03:14 +0200

    add route that will display the same thing always

--- main.py
+++ main.py
@@ -8,6 +8,20 @@ app = FastAPI()
 
 
 # route that will return the collection name
+@app.get("/medical", tags=["Medical"])
+def get_main_category():
+    emergency = "Pediatric Emergency"
+    info = "Medical Information"
+    emergency_photo = medical_info.get_image(emergency)
+    info_photo = medical_info.get_image(info)
+    main_category = [
+        {"medical category name": emergency, "medical category image": emergency_photo},
+        {"medical category name": info, "medical category image": info_photo},
+    ]
+    return main_category
+
+
+# route that will return the collection name
 @app.get("/diseases", response_model=schemas.IllnessCategory, tags=["Illness Category"])
 def get_category():
     return medical_info.get_illnesses_types()
