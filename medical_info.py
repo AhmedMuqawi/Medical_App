9c79bfdccc926c3676c1cd60f2175ea6115534d0
AhemdMuqawi <ahmedmuqawi@gmail.com>
2023-12-31 17:29:31 +0200

    update crud functions to handle image part

--- medical_info.py
+++ medical_info.py
@@ -3,24 +3,47 @@ from typing import List, Dict
 from . import schemas
 
 db = database.db
+execpts = ["Photos", "Pediatric Emergency"]
 
 
 def get_image(image_name: str) -> str:
     photos_collection = db["Photos"]
     image_url = photos_collection.find_one({image_name: {"$exists": True}})
+    # if image_url is None:
+    #     return "No Photo"
     return image_url.get(image_name)
 
 
-def get_illnesses_types() -> list[str]:
+def get_illnesses_types() -> List[schemas.IllnessCategory]:
     collections_list = db.list_collection_names()
-    return schemas.IllnessCategory(illness_type=collections_list)
+    excepts = ["Photos", "Pediatric Emergency"]
+    illness_types_with_images = []
+
+    for collection_name in collections_list:
+        if collection_name not in excepts:
+            image_url = get_image(collection_name)
+            illness_data = schemas.IllnessCategory(
+                illness_type=collection_name, illness_type_image=image_url
+            )
+            illness_types_with_images.append(illness_data)
+
+    return illness_types_with_images
 
 
-def get_illnesses_names(collection_name: str) -> List[str]:
+def get_illnesses_names(collection_name: str) -> List[schemas.IllnessNames]:
     collection = db.get_collection(collection_name)
     documents = list(collection.find({}, {"_id": 0}))
-    all_keys = [key for doc in documents for key in doc.keys()]
-    return schemas.IllnessNames(illnesses=all_keys)
+    illness_names_with_images = []
+
+    for doc in documents:
+        for illness_name, _ in doc.items():
+            image_url = get_image(illness_name)
+            illness_data = schemas.IllnessNames(
+                illness_name=illness_name, illness_image=image_url
+            )
+            illness_names_with_images.append(illness_data)
+
+    return illness_names_with_images
 
 
 def get_illness_info(collection_name: str, document_key: str) -> Dict:
@@ -47,4 +70,8 @@ def get_illness_info(collection_name: str, document_key: str) -> Dict:
 #     print("*" * 50)
 # print(get_illnesses_names("Pediatric_Emergency"))
 # print(get_illness_info("Urinary_Tract_diseases", "Urinary Tract Infection"))
-print(get_image("Pediatric Emergency"))
+# print(get_image("Pediatric Emergency"))
+# print(len(get_illnesses_types()))
+# temp = get_illnesses_types()
+# for i in temp:
+#     print(i)
