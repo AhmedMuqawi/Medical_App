1016179000355ee7e3ca6960df577887388d49d2
AhmedMuqawi <ahmedmuqawi@gmail.com>
2024-02-19 22:21:21 +0200

    add some lines that will seperates the developing operation from the production one

--- app/database.py
+++ app/database.py
@@ -5,9 +5,14 @@ from pymongo import MongoClient
 #     "MONGO_PORT", "27017"
 # )  # Get port from environment variable, default to 27017
 
-mongoDB_url = "mongodb://mongodb:27017/"
+
+##for production
+# mongoDB_url = "mongodb://mongodb:27017/"
+# client = MongoClient(mongoDB_url)
 
 
+#for develpoing
+mongoDB_url = "mongodb://127.0.0.1:27017/"
 client = MongoClient(mongoDB_url)
 
 # Access the database
