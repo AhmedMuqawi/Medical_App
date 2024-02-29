781d8bc53bee79822a49c7f2a7d0be1bcbbd9d9f
AhmedMuqawi <ahmedmuqawi@gmail.com>
2024-02-29 11:44:34 +0200

    save the final edit

--- app/database.py
+++ app/database.py
@@ -6,14 +6,14 @@ from pymongo import MongoClient
 # )  # Get port from environment variable, default to 27017
 
 
-# #for production
-# mongoDB_url = "mongodb://mongodb:27017/"
-# client = MongoClient(mongoDB_url)
-
-
-#for develpoing
-mongoDB_url = "mongodb://127.0.0.1:27017/"
+#for production
+mongoDB_url = "mongodb://mongodb:27017/"
 client = MongoClient(mongoDB_url)
+
+
+# #for develpoing
+# mongoDB_url = "mongodb://127.0.0.1:27017/"
+# client = MongoClient(mongoDB_url)
 
 # Access the database
 db = client["MedicalInfoDB"]
